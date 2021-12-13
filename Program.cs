5e382884ae5ad38608b223c4cdf2be381978ce3b
Wallace Tácio <wallacett.dev@gmail.com>
2021-12-12 22:11:35 -0300

    Adiciona resolução do Projeto Final ETEC 2021

---
+++ Program.cs
@@ -0,0 +1,234 @@
+﻿using System;
+
+namespace ProjetoFinal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcaoSelecao = -1;
+            menu();
+            do{
+                opcaoSelecao = opcaoGeral();
+            executarOpcao(opcaoSelecao);
+            }while(opcaoSelecao != 0);
+
+        }
+
+        static void creditos()
+        {
+            Console.WriteLine(@"Criador: Wallace Tácio Teixeira
+Site: https://github.com/wallacetacio/ProjetoFinal?classId=0f526fae-1786-4ae1-a2b4-b585a14065ac&assignmentId=9c46b86e-7a00-4cbd-8b51-13a51fe59639&submissionId=4000287d-ae6e-3408-690a-df4157ae9b3d");
+        }
+
+        static void executarOpcao(int opcao)
+        {
+            switch(opcao)
+            {
+                case 1:
+                Soma();
+                break;
+                case 2:
+                Subtracao();
+                break;
+                case 3:
+                Mutiplicacao();
+                break;
+                case 4:
+                Divisao();
+                break;
+                case 5:
+                radiciacao();
+                break;
+                case 6:
+                Exponenciacao();
+                break;
+                case 7:
+                creditos();
+                break;
+                case 0:
+                Console.WriteLine("Sair");
+                break;                
+                default:
+                Console.WriteLine("Opcão inválida!");
+                break;
+            }
+        }
+
+        static void Exponenciacao()
+        {
+            double basResult;
+
+            Console.Write("Digite a base: ");
+            double bas = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o expoente: ");
+            double expn =  Convert.ToDouble(Console.ReadLine());
+
+            basResult = Math.Pow(bas,expn);
+
+            Console.WriteLine($"O resultado é {basResult}");
+        }
+
+        static void radiciacao()
+        {
+            Console.Write("Digite o radicando: ");
+            double radicando = Convert.ToDouble(Console.ReadLine());
+
+            double raizQuadrada = Math.Sqrt(radicando);
+
+            Console.WriteLine($"O resultado é: {raizQuadrada}");
+        }
+
+        static void Divisao()
+        {
+            Console.Write("Digite o dividendo/numerador: ");
+            decimal numerador = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Digite o divisor/denominador: ");
+            decimal denominador = Convert.ToDecimal(Console.ReadLine());
+
+            decimal quociente = numerador / denominador;
+
+            decimal resto = numerador % denominador;
+
+            Console.WriteLine($"O quociente da divisão é: {quociente} e seu resto é: {resto}");
+        }
+
+        static void Mutiplicacao()
+        {
+            Console.Write("Digite a quantidade de números: ");
+            int qnt = Convert.ToInt32(Console.ReadLine());
+            int exb = 0;
+            
+            int mutiplicacao = 1;
+
+            if (qnt <= 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("ERRO! ");
+                Console.ResetColor();
+                Console.WriteLine("Quantidade digitada deve ser maior que zero.");
+            }
+            else
+            {
+
+                while (exb < qnt)
+                {
+                    exb = exb + 1;
+
+                    Console.Write($"Número #0{exb}: ");
+
+                    int valor = Convert.ToInt32(Console.ReadLine());
+
+                    mutiplicacao = valor * mutiplicacao;
+                }
+                Console.Write("Mutiplicação é igual a: ");
+                Console.WriteLine(mutiplicacao);
+            }
+        
+        }
+
+        static void Subtracao()
+        {
+            Console.Write("Digite a quantidade de números: ");
+            int qnt = Convert.ToInt32(Console.ReadLine());
+            int exb = 0;
+            
+            int subtracao = 0;
+
+            if (qnt <= 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("ERRO! ");
+                Console.ResetColor();
+                Console.WriteLine("Quantidade digitada deve ser maior que zero.");
+            }
+            else
+            {
+
+                while (exb < qnt)
+                {
+                    exb = exb + 1;
+
+                    Console.Write($"Número #0{exb}: ");
+
+                    int valor = Convert.ToInt32(Console.ReadLine());
+
+                    subtracao = Math.Abs(subtracao) - Math.Abs(valor);
+                }
+                Console.Write("Subtração é igual a: ");
+                Console.WriteLine(subtracao);
+            }
+        
+        }
+        static void Soma()
+        {
+            Console.Write("Digite a quantidade de números: ");
+            int qnt = Convert.ToInt32(Console.ReadLine());
+            int exb = 0;
+            int valor;
+            int soma = 0;
+
+            if (qnt <= 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("ERRO! ");
+                Console.ResetColor();
+                Console.WriteLine("Quantidade digitada deve ser maior que zero.");
+            }
+            else
+            {
+
+                while (exb < qnt)
+                {
+                    exb = exb + 1;
+
+                    Console.Write($"Número #0{exb}: ");
+
+                    valor = Convert.ToInt32(Console.ReadLine());
+
+                    soma = soma + valor;
+                }
+                Console.Write("Soma é igual a: ");
+                Console.WriteLine(soma);
+            }
+        
+        }
+
+        static void menu()
+        {
+                string menu = @"
+            *===================*
+            | Menu de Opções    |
+            *===================*
+            | 1 - Soma          |
+            | 2 - Subtração     |
+            | 3 - Multiplicação |
+            | 4 - Divisão       |
+            | 5 - Radiciação    |
+            | 6 - Exponenciação |
+            | 7 - Créditos      |
+            | 0 - Sair          |
+            *===================*";
+            Console.WriteLine(menu);
+        }
+
+        static int opcaoGeral()
+        {
+            
+            Console.Write("Digite o número referente a opção: ");
+            string opcaoDigitada = Console.ReadLine();
+            int number;
+            bool itsNumber = Int32.TryParse(opcaoDigitada, out number);
+            if(itsNumber)
+            {
+                return number;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # ProjetoFinal
-Projeto Final ETEC 2021
+
+Esse programa executa cálculos matemáticos: soma, subtração, multiplicação, divisão, radiciação e exponenciação.
+
+## _Download_
+
+Baixe o arquivo abaixo. Descompacte na pasta desejada.
+
+[Download do arquivo .zip](dist/ProjetoFinalWallace.zip)
+
+Execute utilizando o comando:
+```
+dotnet ProjetoFinal.dll
+```
+
+Ou se você estiver no Windows, dê um duplo-clique no icone do programa
+
+## Agradecimentos
+[Etec Adolfo Berezin](http://eteab.com.br/cms/)
+[Professor Ermogenes](https://github.com/ermogenes)
+[Professor Diego Neri](diego.neri@etec.sp.gov.br)
